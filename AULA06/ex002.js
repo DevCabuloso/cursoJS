00d7d605828ea14c420c9f86a59601a59f350116
Karlos Eduardo Nunes de Souza <karlosnunesdasilva4@gmail.com>
2024-10-11 16:35:36 -0300

    estudosJS

---
+++ AULA06/ex002.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é seu nome?') // Vai aparecer o nome... //
+var idade = window.prompt('qual sua idade?') //Vai aparercer a idade//
+var pais = window.prompt('Qual seu pais?')
+window.alert('É um grande prazer te conhecer, ' + nome + ' Você tem ' + idade + ' anos e mora em ' + pais + '.');
